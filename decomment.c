e5fa2295771911e86be4b44756f51a4938b6176a
Kevvy <25ktran8@gmail.com>
2026-02-09 19:56:31 -0500

    1st attempt at error handler

--- decomment.c
+++ decomment.c
@@ -10,7 +10,7 @@ enum Statetype
 handleNormalState(int c) {
     enum Statetype state;
     if (c== '/') {
-        // replace char with nothing, must see if it's a comment first
+        /* replace char with nothing, must see if it's a comment first */
         state = MAYBE_COMMENT_START;
     } else if (c == '\'') {
         state = CHAR_START;
@@ -29,11 +29,11 @@ handleNormalState(int c) {
 enum Statetype
 handleMaybeCommentStartState(int c) {
     enum Statetype state;
-    if (c == '/') { // should put down another / since one is missing from earlier
+    if (c == '/') { /* should put down another / since one is missing from earlier */
         state = MAYBE_COMMENT_START;
         putchar(c);
     } else if (c == '*') {
-    // comment marker found, replace with a space
+    /* comment marker found, replace with a space */
         state = COMMENT_START;
         putchar(' ');
     } else if (c == '\'') {
@@ -44,7 +44,7 @@ handleMaybeCommentStartState(int c) {
         putchar(c);
     }
     else {
-        // not a comment so we must put back / from earlier
+        /* not a comment so we must put back / from earlier */
         putchar('/');
         putchar(c);
         state = NORMAL;
@@ -54,9 +54,9 @@ handleMaybeCommentStartState(int c) {
 enum Statetype
 handleCommentStartState(int c) {
     enum Statetype state;
-    if (c == '*') { // remove just in case it is the end of comment
+    if (c == '*') { /* remove just in case it is the end of comment */
         state = MAYBE_COMMENT_END;
-    } else { // still in comment, keep putting down characters
+    } else { /* still in comment, keep putting down characters */
         state = COMMENT_START;
     }
     return state;
@@ -64,11 +64,11 @@ handleCommentStartState(int c) {
 enum Statetype
 handleMaybeCommentEndState(int c) {
     enum Statetype state;
-    if (c == '*') { // still possible to terminate comment, put down * since we removed one earlier
+    if (c == '*') { /* still possible to terminate comment, put down * since we removed one earlier */
         state = MAYBE_COMMENT_END;
-    } else if (c == '/') { // comment ends
+    } else if (c == '/') { /*comment ends */
         state = NORMAL;
-    } else { // false alarm, comment continues
+    } else { /* false alarm, comment continues */
         state = COMMENT_START;
     }
     return state;
@@ -76,12 +76,12 @@ handleMaybeCommentEndState(int c) {
 enum Statetype
 handleCharStartState(int c) {
     enum Statetype state;
-    if (c == '\\') { // escape char detected, next char doesn't mean anything
+    if (c == '\\') { /* escape char detected, next char doesn't mean anything */
         state = CHAR_ESCAPE;
-    } else if (c == '\'') { // end of char
+    } else if (c == '\'') { /* end of char */
         state = NORMAL;
     }
-    else { // char has not terminated
+    else { /* char has not terminated */
         state = CHAR_START;
     }
     putchar(c);
@@ -91,17 +91,17 @@ enum Statetype
 handleCharEscapeState(int c) { 
     enum Statetype state;
     state = CHAR_START;
-    putchar(c); // char escape so just put down whatever input and go back to char
+    putchar(c); /* char escape so just put down whatever input and go back to char */
     return state;
 }
 enum Statetype
 handleStringStartState(int c) {
     enum Statetype state;
-    if (c == '\\') { // escape char detected, next char doesn't mean anything
+    if (c == '\\') { /* escape char detected, next char doesn't mean anything */
         state = STRING_ESCAPE;
-    } else if (c == '\"') { // end of string
+    } else if (c == '\"') { /* end of string */
         state = NORMAL;
-    } else { // string has not terminated
+    } else { /* string has not terminated */
         state = STRING_START;
     }
     putchar(c);
@@ -111,7 +111,7 @@ enum Statetype
 handleStringEscapeState(int c) { 
     enum Statetype state;
     state = STRING_START;
-    putchar(c); // char escape so just put down whatever input and go back to string
+    putchar(c); /* char escape so just put down whatever input and go back to string */
     return state;
 }
 
@@ -146,6 +146,8 @@ int main(void) {
                 break;
         }
     }
-    
-    return 0;
+    if (state == NORMAL ||state == MAYBE_COMMENT_START || state == CHAR_START || state == CHAR_ESCAPE || state == STRING_START || state == STRING_ESCAPE) {
+        return 0;
+    }
+    return 1;
 }
